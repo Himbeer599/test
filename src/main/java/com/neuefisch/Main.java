3c46872bdca95c035986f5d11540771315d6479f
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-13 10:50:08 +0100

    sum-cal

--- src/main/java/com/neuefisch/Main.java
+++ src/main/java/com/neuefisch/Main.java
@@ -3,5 +3,9 @@ package com.neuefisch;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a = 1;
+        int b = 3;
+        int sum = a+b;
+        System.out.println("sum: "+sum);
     }
 }
